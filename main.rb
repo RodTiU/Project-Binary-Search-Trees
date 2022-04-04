2cb605990d0eb0299919a9ad5e9aa37124018f46
Rodolfo <rudolfrce@gmail.com>
2022-04-04 13:37:55 -0600

    Add nil print option when no data in root

--- main.rb
+++ main.rb
@@ -31,6 +31,8 @@ class Tree
   end
 
   def insert(value, root = @root)
+    return @root.data = nil if @root.data.nil?
+
     if value == root.data
       nil
     elsif value < root.data
@@ -51,10 +53,10 @@ class Tree
   end
 
   def delete(value, root = @root)
+    return @root.data = nil if @root.left.nil? && @root.right.nil? && @root.data == value
+
     if root == Node.new(value) || root.left == Node.new(value) || root.right == Node.new(value)
       if root == Node.new(value)
-        # return @root = nil if root.left == nil && root.right == nil
-
       elsif root.left == Node.new(value)
         return root.left = nil if root.left.left.nil? && root.left.right.nil?
         return root.left = root.left.right if root.left.left.nil? && !root.left.right.nil?
@@ -76,17 +78,20 @@ class Tree
   end
 
   def pretty_print(node = @root, prefix = "", is_left = true)
+    return p nil if @root.data == nil
     pretty_print(node.right, "#{prefix}#{is_left ? "│   " : "    "}", false) if node.right
     puts "#{prefix}#{is_left ? "└── " : "┌── "}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? "    " : "│   "}", true) if node.left
   end
 end
 
-tr = Tree.new([1, 2, 3, 6, 100, 10])
-tr.insert(15)
-tr.insert(17)
-tr.insert(21)
-tr.insert(7)
-tr.insert(8)
-tr.insert(9)
+tr = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+# tr.insert(15)
+# tr.insert(17)
+# tr.insert(21)
+# tr.insert(7)
+# tr.insert(8)
+# tr.insert(9)
+# tr.delete(1)
+# tr.insert(2)
 tr.pretty_print
